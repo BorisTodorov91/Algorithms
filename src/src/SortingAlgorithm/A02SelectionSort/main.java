89c12248c75b94d7594e902fcf6565ed25aadc2b
Boris <b.todorov91@gmail.com>
2023-03-03 00:03:02 +0200

    new upp

--- src/src/SortingAlgorithm/A02SelectionSort/main.java
+++ src/src/SortingAlgorithm/A02SelectionSort/main.java
@@ -6,6 +6,10 @@ public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        SelectionSort ob = new SelectionSort();
+        int arr[] = {64,25,12,22,11};
+        ob.sort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
     }
 }
